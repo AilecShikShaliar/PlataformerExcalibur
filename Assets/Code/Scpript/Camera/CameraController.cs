17de47406ad37fab7ab712a17005486fcfca4369
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2024-02-20 12:39:12 +0100

    Movimiento
    Movimiento del personajes arreglado.
    Tengo que arreglar la cámara y el collaider

---
+++ Assets/Code/Scpript/Camera/CameraController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //POSICION DEL OBJETIVO DE LA CAMARA
+    public Transform target;
+
+    //POSICION MINIMA Y VERTICAL 
+    public float minHeight, maxHeight;
+
+    //POSICION DE LOS FONDOS
+    public Transform farBg, middleBg;
+
+    //REFERENCIA A LA ULTIMA POSICIÓN DEL JUGADOR
+    private Vector2 _lastPos;
+
+
+    void Start()
+    {
+        _lastPos = transform.position;
+    }
+
+
+    private void LateUpdate()
+    {
+       //SIGUE AL JUGADO SIN VARIAR POSICION Z
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+        
+        //CUANTO HAY QUE MOVERSE EN LA X
+        Vector2 _amountToMove = new Vector2(transform.position.x - _lastPos.x, transform.position.y - _lastPos.y);
+        
+        //COMO SE MUEVE EL FONDO A LA MISMA VELOCIDAD DEL JUGADOR
+        farBg.position = farBg.position + new Vector3(_amountToMove.x, _amountToMove.y, 0f);
+        middleBg.position += new Vector3(_amountToMove.x, _amountToMove.y, 0f) * .5f;
+
+        //ACTUALIZAR POSICION DEL JUGADOR
+        _lastPos = transform.position;
+
+    }
+}
---
+++ Assets/Code/Scpript/Player/PlayerControllerVal.cs
@@ -0,0 +1,98 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerVal : MonoBehaviour
+{
+    //Velocidad
+    public float moveSpeed;
+    //Salto
+    public float jumpForce;
+
+    //Jugador está en el suelo
+    private bool _isGrounded;
+    //Detectar suelo
+    public Transform graundCheckPoint;
+    //Referencia a las Layer del suelo
+    public LayerMask whatIsGraund;
+    //Doble salto
+    private bool _canDubleJump;
+
+    //RB del jugador
+    private Rigidbody2D _theRB;
+    //Referencia al Animator
+    private Animator _anim;
+    //Referencia al SpriteRenderer
+    private SpriteRenderer _theSR;
+    
+    void Start()
+    {
+        //RB del jugador
+        _theRB = GetComponent<Rigidbody2D>();
+
+        //Animator del jugador 
+        _anim = GetComponent<Animator>();
+
+        //SpriteRendered del jugador
+        _theSR = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        //input = Input.GetAxisRaw("Horizontal");
+
+        _theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, _theRB.velocity.y);
+
+        //_isGrounded = Physics2D.OverlapCircle(graundCheckPoint.position, .2f,whatIsGraund);
+
+        if(Input.GetAxisRaw("Horizontal") !=0f)
+        {
+            _anim.SetFloat("moveSpeed", (Input.GetAxisRaw("Horizontal") * moveSpeed));
+        }
+        else
+        {
+            _anim.SetFloat("moveSpeed", 0f);
+        }
+
+             //BOTON DE SALTO
+             if (Input.GetButtonDown("Jump"))
+                { 
+                    //si el jugador esté en el suelo
+                    if (_isGrounded)
+                    {
+                        _theRB.velocity = new Vector2(_theRB.velocity.x, jumpForce);
+
+                        _canDubleJump = true;
+                    }
+
+                    else
+                    { 
+                        if (_canDubleJump)
+                        {
+                            _theRB.velocity = new Vector2(_theRB.velocity.x, jumpForce);
+
+                            _canDubleJump = false;
+                        }
+                    }
+
+                }
+
+        Debug.Log("velocidad: " + (Input.GetAxisRaw("Horizontal") * moveSpeed));
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "ground")
+            {
+                _isGrounded = true;
+            }
+            else _isGrounded = false;
+        }
+
+
+        _anim.SetBool("isGrounded", _isGrounded);
+    }
+
+
+
+}
