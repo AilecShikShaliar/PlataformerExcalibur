e8438c7825c79647a4cc6dbfc526b2a823ba436e
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2024-03-13 13:32:57 +0100

    Mejora de escenario
    Se mejoró algunos aspectos del último escenario. Se han añadido algunos paneles. Mirar selector de niveles

--- Assets/Code/Scpript/Manager/LoadLevelManager.cs
+++ Assets/Code/Scpript/Manager/LoadLevelManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadLevelManager : MonoBehaviour
 {
+    private PlayerControllerVal _Ls;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,8 @@ public class LoadLevelManager : MonoBehaviour
     {
         
     }
+
+
+
+
 }
