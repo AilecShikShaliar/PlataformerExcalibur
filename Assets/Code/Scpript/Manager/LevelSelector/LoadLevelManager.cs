3ffcd8e78c4eef548c6e20d411ccd9044531eed6
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2024-03-13 21:19:25 +0100

    Intento de levelSelector
    No funcionó

---
+++ Assets/Code/Scpript/Manager/LevelSelector/LoadLevelManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelManager : MonoBehaviour
+{
+    private LSUIController  _lSUIReference;
+
+    //REFERENCIA AL JUGADOR
+    private LSPlayer _lS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //INICIALICIZAMOS SCRIPT JUGADOR
+        _lS = GameObject.Find("PlayerControllerVal").GetComponent<LSPlayer>();
+
+        //INICIALICIZAMOS SCRIPT LSUICONTROLLER
+       _lSUIReference = GameObject.Find("LSCanvas").GetComponent<LSUIController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel()
+    {
+        //LLAMAMOS A LA CORRUTINA PARA CARGAR EL NIVEL
+        StartCoroutine(LoadLevelCo());
+    }
+
+    public IEnumerator LoadLevelCo()
+    {
+        //FUNDIDO A NEGRO
+        _lSUIReference.FadeToBlack();
+        //ESPERAR TIEMPO DETERMINADO
+        yield return new WaitForSeconds(1f);
+        //CARAGAMOS EL NIVEL 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+            SceneManager.LoadScene(_lS.currentPoint.levelToLoad);
+
+        }
+        
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Code/Scpript/Manager/LevelSelector/MapPoint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    #region VARIABLES
+    //MAP POINTS
+    public MapPoint one, two;
+
+    //RECONOCIMIENTO DEL NIVEL
+    public bool isLevel;
+
+    //RECONOCIMIENTO DE CARGA 
+    public string levelToLoad;
+
+    #endregion
+
+    #region UNITY METHODS
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+}
