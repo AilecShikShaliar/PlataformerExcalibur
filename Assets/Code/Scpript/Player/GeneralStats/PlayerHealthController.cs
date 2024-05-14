3e6ef5e15860796bf2c28c93e399348ee8a09684
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2024-05-14 19:46:40 +0200

    barra de vida

--- Assets/Code/Scpript/Player/GeneralStats/PlayerHealthController.cs
+++ Assets/Code/Scpript/Player/GeneralStats/PlayerHealthController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealthController : MonoBehaviour
-   
 {
-    [HideInInspector] public int currentHealth;
+    [SerializeField] private float vida;
+    [SerializeField] private float maximoVida;
 
+    [SerializeField] private LifeBar _lifeBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        vida = maximoVida;
+
+        _lifeBar =  GameObject.Find("life").GetComponent<LifeBar>();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void Daño(float daño)
+   {
+     vida -= daño;
+     _lifeBar.CambiarVidaActual(vida);
+     if (vida <= 0) Destroy(gameObject);
+   }
+
+
 }
